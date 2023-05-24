95ccf8e0b95d919de82557a623434fcce561438f
painter98 <emailmepallavi@gmail.com>
2023-05-24 13:56:07 +0530

    solving 2

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,16 @@ const App = () => {
   let [text,useText] = useState("");
 
   function textInput(e){
-    useText("Hello "+e.target.value);
+    useText("Hello "+e.target.value+"!");
   }
 
   return (
     <div>
         {/* Do not remove the main div */}
         <p>Enter your name:</p>
-        <input onChange={textInput} type='text'/>
-        <p>{text}</p>
+        <input onChange={textInput} type='text'style={{display:"block"}}/>
+        <br/>
+        {text}
     </div>
   )
 }
